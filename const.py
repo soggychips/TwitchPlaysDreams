8365a9c0603abf0aa24968bc082469970f355bd9
Jake <jake_cohen@me.com>
2021-06-15 15:33:12 -0700

    updated gitignore and const file

---
+++ const.py
@@ -0,0 +1,25 @@
+buttons = {
+    "touchpad": 1 << 1,
+    "square": 1 << 4,
+    "x": 1 << 5,
+    "circle": 1 << 6,
+    "triangle": 1 << 7,
+    'l1': 1 << 8,
+    'r1': 1 << 9,
+    'l3' : 1 << 14,
+    'r3': 1 << 15
+}
+
+dpad_directions = {
+    'up': 0x0,
+    'u': 0x0,
+    'right': 0x2,
+    'r': 0x2,
+    'down': 0x4,
+    'd': 0x4,
+    'left': 0x6,
+    'l': 0x6,
+    # None : 0x8
+}
+
+triggers = ['r2', 'l2']
