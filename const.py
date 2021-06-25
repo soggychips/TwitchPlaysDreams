25c07186aba107e0dfc9311cdc39f076de79248b
Jake <jake_cohen@me.com>
2021-06-25 13:24:10 -0700

    added more controls (including sticks) to const

--- const.py
+++ const.py
@@ -4,18 +4,20 @@ buttons = {
     "x": 1 << 5,
     "circle": 1 << 6,
     "triangle": 1 << 7,
-    'l1': 1 << 8,
-    'r1': 1 << 9,
-    'l3' : 1 << 14,
-    'r3': 1 << 15
+    "l1": 1 << 8,
+    "r1": 1 << 9,
+    "l3": 1 << 14,
+    "r3": 1 << 15,
 }
 
 dpad_directions = {
-    'up': 0x0,
-    'right': 0x2,
-    'down': 0x4,
-    'left': 0x6,
+    "up": 0x0,
+    "right": 0x2,
+    "down": 0x4,
+    "left": 0x6,
     # None : 0x8
 }
 
-triggers = ['r2', 'l2']
+triggers = ["r2", "l2"]
+
+sticks = ['l', 'r']
