a8353ea680819fddbb33f991272208a642aa92c7
Jake <jake_cohen@me.com>
2021-06-25 13:24:58 -0700

    added sticks functions, created a more generic button call that can tap or hold, added seconds to all calls to allow for 'taps' or holds

--- Controller.py
+++ Controller.py
@@ -32,17 +32,56 @@ class Controller:
             self.gamepad.update()
             sleep(Controller.two_frames)
 
-    def dpad(self, direction):
+    def button(self, button, seconds=0.0):
+        with self.lock:
+            self.gamepad.press_button(button)
+            self.gamepad.update()
+            sleep(Controller.one_frame if seconds == 0.0 else seconds)
+            self.gamepad.release_button(button)
+            self.gamepad.update()
+            sleep(Controller.two_frames)
+
+    def dpad(self, direction, seconds=0.0):
         """Emulates tapping of a dpad direction"""
         with self.lock:
             self.gamepad.directional_pad(direction)
             self.gamepad.update()
-            sleep(Controller.one_frame)
+            sleep(Controller.one_frame if seconds == 0.0 else seconds)
             self.gamepad.directional_pad(0x8)  # dpad None
             self.gamepad.update()
             sleep(Controller.two_frames)
 
-    def trigger(self, which_trigger, amount=1.0):
+    def stick(self, stick, x_value_float=0.0, y_value_float=0.0, seconds=0.0):
+        stick = (
+            self.gamepad.left_joystick_float
+            if stick == "l"
+            else self.gamepad.right_joystick_float
+            if stick == "r"
+            else None
+        )
+        if stick is None:
+            raise Exception()
+        with self.lock:
+            stick(x_value_float=x_value_float, y_value_float=y_value_float)
+            self.gamepad.update()
+            sleep(Controller.one_frame if seconds == 0.0 else seconds)
+            stick(x_value_float=0.0, y_value_float=0.0)
+            self.gamepad.update()
+            sleep(Controller.two_frames)
+
+    def sticks(self, left_amounts=[0.0, 0.0], right_amounts=[0.0, 0.0], seconds=0.0):
+        zeros = [0.0, 0.0]
+        with self.lock:
+            self.gamepad.left_joystick_float(*left_amounts)
+            self.gamepad.right_joystick_float(*right_amounts)
+            self.gamepad.update()
+            sleep(Controller.one_frame if seconds == 0.0 else seconds)
+            self.gamepad.left_joystick_float(*zeros)
+            self.gamepad.right_joystick_float(*zeros)
+            self.gamepad.update()
+            sleep(Controller.two_frames)
+
+    def trigger(self, which_trigger, amount=1.0, seconds=0.0):
         """Emulates a trigger pull (and release) for given amount"""
         trigger_pull = (
             self.gamepad.right_trigger_float
@@ -55,21 +94,8 @@ class Controller:
             raise Exception()
         with self.lock:
             trigger_pull(value_float=amount)
-            sleep(Controller.one_frame)
+            self.gamepad.update()
+            sleep(Controller.one_frame if seconds == 0.0 else seconds)
             trigger_pull(value_float=0.0)
+            self.gamepad.update()
             sleep(Controller.two_frames)
-
-
-if __name__ == "__main__":
-    from const import buttons
-
-    controller = Controller()
-    sleep(Controller.two_frames)
-
-    controller.left()
-    controller.right()
-    controller.left()
-    controller.right()
-    controller.tap(buttons["circle"])
-    controller.tap(buttons["square"])
-    controller.tap(buttons["x"])
